099a43150589c755e4ca65faa2420925971e4263
Marianna Repka <repka.mv@gmail.com>
2025-06-25 01:35:23 +0300

    Solution

---
+++ cypress/e2e/hermioneAccount.cy.js
@@ -0,0 +1,69 @@
+import { faker } from '@faker-js/faker';
+/// <reference types='cypress' />
+
+describe('Bank app', () => {
+  const depositAmount = `${faker.number.int({ min: 500, max: 1000 })}`;
+  const withdrawAmount = `${faker.number.int({ min: 50, max: 500 })}`;
+  const firstBalance = 5096;
+  const expectedBalance = Number(depositAmount) + Number(firstBalance);
+  const balance = expectedBalance - withdrawAmount;
+  const user = 'Hermoine Granger';
+  const accountNumber = '1001';
+  const now = new Date();
+  const todayFormatted = now.toISOString().slice(0, 16);
+
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should provide the ability to work with Hermione\'s bank account', () => {
+    cy.contains('.btn', 'Customer Login').click();
+    cy.get('[name="userSelect"]').select(user);
+    cy.contains('.btn', 'Login').click();
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', accountNumber)
+      .should('be.visible');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', '5096')
+      .should('be.visible');
+    cy.contains('.ng-binding', 'Dollar')
+      .should('be.visible');
+
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[placeholder="amount"]').type(depositAmount);
+    cy.contains('[type="submit"]', 'Deposit').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain', 'Deposit Successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', expectedBalance)
+      .should('be.visible');
+
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.contains('[type="submit"]', 'Withdraw')
+      .should('be.visible');
+    cy.get('[placeholder="amount"]').type(withdrawAmount);
+    cy.contains('[type="submit"]', 'Withdraw').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain', 'Transaction successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', balance)
+      .should('be.visible');
+
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('table').should('be.visible');
+    cy.get('#start').type(todayFormatted);
+    cy.get('td.ng-binding').should('contain', withdrawAmount);
+    cy.get('td.ng-binding').should('contain', depositAmount);
+    cy.get('[ng-click="back()"]').click();
+    cy.get('#accountSelect').select('1002');
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('table').should('be.visible');
+    cy.get('tbody tr').should('not.exist');
+
+    cy.contains('button', 'Logout').click();
+    cy.get('#userSelect').should('have.value', '');
+  });
+});
